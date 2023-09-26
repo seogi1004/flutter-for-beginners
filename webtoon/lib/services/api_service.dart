d97d162936a0b5b328582522e8df6bed6ab3c7e6
alvin <alvin@jennifersoft.com>
2023-09-26 17:40:59 +0900

    웹툰 상세, 에피소드 목록 서비스 구현

--- webtoon/lib/services/api_service.dart
+++ webtoon/lib/services/api_service.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_detail_model.dart';
+import 'package:webtoon/models/webtoon_episode_model.dart';
 import 'dart:convert';
-
 import 'package:webtoon/models/webtoon_model.dart';
 
 class ApiService {
@@ -19,6 +20,31 @@ class ApiService {
         webtoonInstances.add(instance);
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$today/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodeById(
+      String id) async {
+    List<WebtoonEpisodeModel> webtoonEpisodes = [];
+    final url = Uri.parse("$baseUrl/$today/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        webtoonEpisodes.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return webtoonEpisodes;
     }
     throw Error();
   }
