39cb087485b4536fbbcb9c1ab48838c861e80148
alvin <alvin@jennifersoft.com>
2023-10-28 13:56:57 +0900

    비디포 플레이어 개발 중

---
+++ vid_player/lib/screen/home_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('asset/img/logo.png');
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: video == null ? renderEmpty() : renderVideo(),
+    );
+  }
+
+  Widget renderEmpty() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: getBoxDecoration(),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(),
+          SizedBox(height: 30.0),
+          _AppName(),
+        ],
+      ),
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFF2A3A7C),
+            Color(0xFF000118),
+          ]),
+    );
+  }
+
+  Widget renderVideo() {
+    return Container();
+  }
+}
