9831ce4805d9a7c9e1d10f373083d1a45937fba2
alvin <alvin@jennifersoft.com>
2023-11-06 23:22:58 +0900

    first commit!

---
+++ video_call/lib/const/agora.dart
@@ -0,0 +1,4 @@
+const APP_ID = 'eb09fafa2f9d401b8557db3c3e405e47';
+const CHANNER_NAME = 'testchannel';
+const TEMP_TOKEN =
+    '007eJxTYJjJt/fDnliR2xUHj7apiAsV31vTdL3f6rr7w+VTox8GpUgrMKQmGVimJaYlGqVZppgYGCZZmJqapyQZJxunmhiYppqY83/1SG0IZGRoWn6YlZEBAkF8boaS1OKS5IzEvLzUHAYGALS2I+M=';
