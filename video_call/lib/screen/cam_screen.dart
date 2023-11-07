134cff08bc7487282ad73489e65a75762bf4d5b3
alvin <alvin@jennifersoft.com>
2023-11-07 17:45:25 +0900

    스크린 위젯 개발 중

---
+++ video_call/lib/screen/cam_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class CamScreen extends StatefulWidget {
+  const CamScreen({super.key});
+
+  @override
+  State<CamScreen> createState() => _CamScreenState();
+}
+
+class _CamScreenState extends State<CamScreen> {
+  Future<bool> init() async {
+    final resp = await [Permission.camera, Permission.microphone].request();
+    final cameraPermission = resp[Permission.camera];
+    final micPermission = resp[Permission.microphone];
+
+    if (cameraPermission != PermissionStatus.granted ||
+        micPermission != PermissionStatus.granted) {
+      throw '카메라 또는 마이크 권한이 없습니다.';
+    }
+
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LIVE'),
+      ),
+      body: FutureBuilder(
+          future: init(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return const Center(
+              child: Text('모든 권한이 있습니다!'),
+            );
+          }),
+    );
+  }
+}
