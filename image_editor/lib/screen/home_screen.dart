e4590893e9d4c07c480c776388a931dd37786e26
alvin <alvin@jennifersoft.com>
2023-11-13 17:20:26 +0900

    툴바 및 이미지 선택 구현

---
+++ image_editor/lib/screen/home_screen.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_editor/component/main_app_bar.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          renderBody(),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppBar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteItem: onDeleteItem,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget renderBody() {
+    if (image != null) {
+      return Positioned.fill(
+        child: InteractiveViewer(
+          child: Image.file(
+            File(image!.path),
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+    } else {
+      return Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.grey,
+          ),
+          onPressed: onPickImage,
+          child: const Text('이미지 선택하기'),
+        ),
+      );
+    }
+  }
+
+  void onPickImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      this.image = image;
+    });
+  }
+
+  void onSaveImage() {}
+
+  void onDeleteItem() {}
+}
