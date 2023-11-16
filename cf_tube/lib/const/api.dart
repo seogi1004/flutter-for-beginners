c721dfed309bd0cb068feaa24d5426381c7aabdc
alvin <alvin@jennifersoft.com>
2023-11-16 17:42:30 +0900

    코팩튜브 개발 완료

---
+++ cf_tube/lib/const/api.dart
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyDQKVz6vISTh69tIm-qeCAnw6HxVhJwiEI';
+const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = 'UCxZ2AlaT0hOmxzZVbF_j_Sw';
