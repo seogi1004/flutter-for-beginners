08436eb26d93d33941a6119f58a655e84ae30c65
alvin <alvin@jennifersoft.com>
2023-12-02 11:06:52 +0900

    17장 개발 완료

--- calendar_scheduler/lib/component/custom_text_field.dart
+++ calendar_scheduler/lib/component/custom_text_field.dart
@@ -5,8 +5,15 @@ import 'package:flutter/services.dart';
 class CustomTextField extends StatelessWidget {
   final String label;
   final bool isTime;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
 
-  const CustomTextField({super.key, required this.label, required this.isTime});
+  const CustomTextField(
+      {super.key,
+      required this.label,
+      required this.isTime,
+      required this.onSaved,
+      required this.validator});
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +30,8 @@ class CustomTextField extends StatelessWidget {
         Expanded(
           flex: isTime ? 0 : 1,
           child: TextFormField(
+            onSaved: onSaved,
+            validator: validator,
             cursorColor: Colors.grey,
             maxLines: isTime ? 1 : null,
             expands: !isTime,
